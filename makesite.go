b6bf9e1bce0c6e084f8e86d756fdd0d0b3597b04
Rediet8abere <redietaberegesesse@gmail.com>
2020-06-05 21:32:51 -0700

    todotmpl

--- makesite.go
+++ makesite.go
@@ -1,22 +1,36 @@
+// https://blog.gopheracademy.com/advent-2017/using-go-templates/
 package main
+
 import (
-	"fmt"
-  "io/ioutil"
+	"html/template"
+	"os"
+	// "fmt"
+  // "io/ioutil"
 )
 
-func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
-	if err != nil {
-			// A common use of `panic` is to abort if a function returns an error
-			// value that we don’t know how to (or want to) handle. This example
-			// panics if we get an unexpected error when creating a new file.
-			panic(err)
-	}
-	fmt.Print(string(fileContents))
+type entry struct {
+  Name string
+  Done bool
+}
 
-	bytesToWrite := []byte("hello\ngo\n")
-  err2 := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
-  if err2 != nil {
-      panic(err)
-  }
+type ToDo struct {
+  User string
+  List []entry
+}
+
+
+func main() {
+
+	// Data 
+	data := ToDo{
+    User: "Rediet",
+    List: []entry{
+        {Name: "Task 1", Done: false},
+        {Name: "Task 2", Done: true},
+        {Name: "Task 3", Done: true},
+    },
+}
+	// Parse tempalte and Execute
+  t := template.Must(template.ParseFiles("todo.tmpl"))
+	t.Execute(os.Stdout, data)
 }
