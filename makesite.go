6389832774493291454006f8507cef8fa0bdd3ee
Rediet8abere <redietaberegesesse@gmail.com>
2020-06-05 17:50:23 -0700

    reading a file

--- makesite.go
+++ makesite.go
@@ -1,28 +1,16 @@
 package main
-
-<<<<<<< HEAD
 import (
-	"io/ioutil"
+	"fmt"
+  "io/ioutil"
 )
 
 func main() {
-
-}
-
-func readFile() string {
 	fileContents, err := ioutil.ReadFile("first-post.txt")
 	if err != nil {
-		panic(err)
+			// A common use of `panic` is to abort if a function returns an error
+			// value that we don’t know how to (or want to) handle. This example
+			// panics if we get an unexpected error when creating a new file.
+			panic(err)
 	}
-
-	return string(fileContents)
+	fmt.Print(string(fileContents))
 }
-
-func renderTemplate()
-=======
-import "fmt"
-
-func main() {
-	fmt.Println("Hello, world!")
-}
->>>>>>> 9514ac8a2c135a448a2b15a4b246dcd5d59ee7bf
