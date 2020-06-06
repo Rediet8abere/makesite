5d2ae4f2a6490039650bbed65754feb58b1934b6
Rediet8abere <redietaberegesesse@gmail.com>
2020-06-05 21:49:21 -0700

    context to templ

--- makesite.go
+++ makesite.go
@@ -1,22 +1,25 @@
 package main
 import (
-	"fmt"
+	// "fmt"
   "io/ioutil"
+	"html/template"
+	"os"
 )
+
+type FileCont struct {
+	Content string
+}
 
 func main() {
 	fileContents, err := ioutil.ReadFile("first-post.txt")
 	if err != nil {
-			// A common use of `panic` is to abort if a function returns an error
-			// value that we don’t know how to (or want to) handle. This example
-			// panics if we get an unexpected error when creating a new file.
 			panic(err)
 	}
-	fmt.Print(string(fileContents))
+	// sending data to struct
+	content := FileCont{
+		Content: string(fileContents),
+	}
 
-	bytesToWrite := []byte("hello\ngo\n")
-  err2 := ioutil.WriteFile("new-file.txt", bytesToWrite, 0644)
-  if err2 != nil {
-      panic(err)
-  }
+	t := template.Must(template.ParseFiles("template.tmpl"))
+	t.Execute(os.Stdout, content)
 }
