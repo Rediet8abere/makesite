130c232f4121da8109d057453cb1fe3675f1e074
Rediet8abere <redietaberegesesse@gmail.com>
2020-06-08 16:02:43 -0700

    switcing to master

--- makesite.go
+++ makesite.go
@@ -12,62 +12,12 @@ type FileCont struct {
 	Content string
 }
 
-// func save(txt string) {
-//
-// 	fileContents, err := ioutil.ReadFile(txt)
-// 	if err != nil {
-// 			panic(err)
-// 	}
-// 	// sending data to struct
-// 	content := FileCont{
-// 		Content: string(fileContents),
-// 	}
-// 	fmt.Println(content)
-//
-// 	t := template.Must(template.ParseFiles("template.tmpl"))
-//
-// 	// reqBodyBytes := new(bytes.Buffer)
-// 	// json.NewEncoder(reqBodyBytes).Encode(content)
-//   t.Execute(w, content)
-//
-// 	// fmt.Println(reqBodyBytes.Bytes())
-// 	// reqBodyBytes.Bytes()
-// 	stringToWrite := string(cont)
-//   err2 := ioutil.WriteFile("first-post.html", stringToWrite, 0644)
-//   if err2 != nil {
-//       panic(err)
-//   }
-//
-// }
-func main() {
-	// save("first-post.txt")
-	// fileContents, err := ioutil.ReadFile("first-post.txt")
-	// if err != nil {
-	// 		panic(err)
-	// }
-	// // sending data to struct
-	// content := FileCont{
-	// 	Content: string(fileContents),
-	// }
-	// fmt.Println(content)
-	//
-	// t := template.Must(template.ParseFiles("template.tmpl"))
-	//
-	// // reqBodyBytes := new(bytes.Buffer)
-	// // json.NewEncoder(reqBodyBytes).Encode(content)
-  // t.Execute(w, content)
-	//
-	// // fmt.Println(reqBodyBytes.Bytes())
-	// // reqBodyBytes.Bytes()
-	// stringToWrite := string(cont)
-  // err2 := ioutil.WriteFile("first-post.html", stringToWrite, 0644)
-  // if err2 != nil {
-  //     panic(err)
-  // }
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+func save(txt string) {
+	fileContents, err := ioutil.ReadFile(txt)
 	if err != nil {
 			panic(err)
 	}
+	// fmt.Println(fileContents)
 	content := FileCont{
 		Content: string(fileContents),
 	}
@@ -75,9 +25,13 @@ func main() {
 	json.NewEncoder(reqBodyBytes).Encode(content)
 	fmt.Println(reqBodyBytes.Bytes())		// fmt.Println(reqBodyBytes.Bytes())
 	bytesToWrite := []byte(reqBodyBytes.Bytes())		// reqBodyBytes.Bytes()
-  err2 := ioutil.WriteFile("first-post.html", bytesToWrite, 0644)
+  err2 := ioutil.WriteFile(txt + ".html", bytesToWrite, 0644)
 	if err2 != nil {
       panic(err)
   }
+}
+func main() {
+	save("first-post.txt")
+
 
 }
