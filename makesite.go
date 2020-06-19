3f2af1ca8675a92845fe804a681572652ed516ae
Rediet8abere <redietaberegesesse@gmail.com>
2020-06-19 16:17:55 -0700

    v1

--- makesite.go
+++ makesite.go
@@ -39,15 +39,23 @@ func save(filename string) {
 	t.Execute(os.Stdout, content)
 	// 4. Write the HTML template to the filesystem to a file. Name it first-post.html
 	t.Execute(file, content)
+
+}
+
+func testFlag() {
+	// https://stackoverflow.com/questions/1726891/explanation-of-flags-in-go#:~:text=flags%20are%20a%20common%20way,options%20for%20command%2Dline%20programs.&text=Try%20out%20the%20run%20program,automatically%20take%20their%20default%20values.&text=If%20you%20provide%20a%20flag,will%20overwrite%20by%20passed%20one.
 	// 5. flag to your command named file. This flag represents the name of any .txt file in the same directory as your program
-	file_flag := flag.String("latest-post.html", "defaultValue", ".txt")
+	fileFlag := flag.String("file", ".txt", "a string")
+	dirFlag := flag.String("dir", "search", "a string")
 	flag.Parse()
-	fmt.Println("file:", *file_flag)
+	fmt.Println("file:", *fileFlag)
+	fmt.Println("looking for txt file:", *dirFlag)
 }
 
 func main() {
 	save("first-post.txt")
+	save("test1.txt")
+	save("test2.txt")
+	save("test3.txt")
+	testFlag()
 }
-
-// Questions: how to write an html file
-// 						what is flag and what does it do?
